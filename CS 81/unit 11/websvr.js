279cb61fd46cfde170ce134a19779e4b0c588e7c
JimmyRowland <tian_yu@student.smc.edu>
2017-07-23 01:07:25 -0700

    unit 11

--- CS 81/unit 11/websvr.js
+++ CS 81/unit 11/websvr.js
@@ -4,7 +4,7 @@
 var http = require("http");
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Hello World, my name is Yu Tian\n');
+    response.write('Hello World, my name is Jimmy\n');
 }).listen(8001);
 
 console.log('Server running at http://127.0.0.1:8001/');
